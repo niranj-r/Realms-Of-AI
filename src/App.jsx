49c02639163abe8e425453a43eddd0e6a99ab805
niranj-r <niranj.njai@gmail.com>
2026-01-05 23:04:30 +0530

    Add RealmsHero component and associated styles
    - Created RealmsHero.jsx component to display the hero section with title and image.
    - Added RealmsHero.css for styling the hero section, including responsive design for mobile devices.
    - Included images for the title and hero character.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+import RealmsHero from "./pages/RealmsHero";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<RealmsHero />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
